68c7b9f5301578c215d10b22a042dda08148e5dd
hacene <zerrouk.mohammed.hacene@gmail.com>
2024-10-28 20:58:15 +0100

    hide date number

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -36,7 +36,7 @@ export default function Welcome({
                 <div className="max-w-7xl mx-auto">
                     <div className="flex items-center justify-between mb-10">
                         <h1 className="text-2xl md:text-3xl font-bold text-right ">
-                            التقارير لهذا الشهر {currentMonth} / {currentYear} 
+                            التقارير لهذا الشهر  
                         </h1>
                         <Button
                             type="button"
